a7c8b6ef97cb669b0c68649f1813d110224521da
kenvilar <kenvilar@gmail.com>
2023-05-06 10:16:50 +0800

    use directives

--- src/app/components/Snippets.tsx
+++ src/app/components/Snippets.tsx
@@ -2,10 +2,8 @@ const Snippets = () => {
   return (
     <>
       <section id="snippets">
-        <div className="max-w-6xl mx-auto text-center my-20 px-10">
-          <h3 className="mb-8 text-4xl font-bold text-darkGrayishBlue md:text-5xl">
-            Keep track of your snippets
-          </h3>
+        <div className="section-container my-20 px-10">
+          <h3>Keep track of your snippets</h3>
           <p className="max-w-3xl mx-auto mb-24 text-xl leading-9 text-center text-grayishBlue">
             Clipboard instantly stores any item you copy in the cloud, meaning
             you can access your snippets immediately on all your devices. Our
