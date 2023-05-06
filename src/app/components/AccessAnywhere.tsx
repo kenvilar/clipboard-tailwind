cce21485642734a58cde83c0fd1fb9c708059630
kenvilar <kenvilar@gmail.com>
2023-05-06 11:01:10 +0800

    update AccessAnywhere component

--- src/app/components/AccessAnywhere.tsx
+++ src/app/components/AccessAnywhere.tsx
@@ -1,7 +1,19 @@
+import Devices from "@/app/assets/images/image-devices.png";
+
 const AccessAnywhere = () => {
   return (
     <>
-      <div>AccessAnywhere</div>
+      <section id="access">
+        <div className="section-container my-20">
+          <h3>Access Clipboard Anywhere</h3>
+          <p className="max-w-3xl mx-auto mb-24 text-xl leading-9 text-center text-grayishBlue">
+            Whether you’re on the go, or at your computer, you can access all
+            your Clipboard snippets in a few simple clicks.
+          </p>
+
+          <img src={Devices.src} alt="img-devices" className="mx-auto" />
+        </div>
+      </section>
     </>
   );
 };
