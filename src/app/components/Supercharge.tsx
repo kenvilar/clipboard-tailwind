01138a909afb0509c6510f3bd99b4104f2a1dbb8
kenvilar <kenvilar@gmail.com>
2023-05-06 11:22:46 +0800

    update Supercharge component

--- src/app/components/Supercharge.tsx
+++ src/app/components/Supercharge.tsx
@@ -1,4 +1,27 @@
+import Image from "next/image";
+import IconBlacklist from "@/app/assets/images/icon-blacklist.svg";
+import IconText from "@/app/assets/images/icon-text.svg";
+import IconPreview from "@/app/assets/images/icon-preview.svg";
+
 const Supercharge = () => {
+  const items = [
+    {
+      icon: IconBlacklist,
+      title: "Create Blacklists",
+      desc: "Easily search your snippets by content, category, web address, application, and more.",
+    },
+    {
+      icon: IconText,
+      title: "Plain Text Snippets",
+      desc: "Remove unwanted formatting from copied text for a consistent look.",
+    },
+    {
+      icon: IconPreview,
+      title: "Sneak Preview",
+      desc: "Quick preview of all snippets on your Clipboard for easy access.",
+    },
+  ];
+
   return (
     <>
       <section id="supercharge">
@@ -7,6 +30,18 @@ const Supercharge = () => {
           <p className="section-content mb-16">
             We{`'`}ve got the tools to boost your productivity.
           </p>
+
+          <div className="flex flex-col items-center justify-between space-y-16 md:flex-row md:space-y-0 md:space-x-12">
+            {items.map((item: any, idx) => {
+              return (
+                <div key={idx} className="flex flex-col items-center space-y-5">
+                  <Image src={item.icon} alt="blacklist" className="mb-6" />
+                  <h5>{item.title}</h5>
+                  <p className="max-w-md text-grayishBlue">{item.desc}</p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
