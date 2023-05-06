df97e9da031e6712437e6cd8e16f4010b996a042
kenvilar <kenvilar@gmail.com>
2023-05-06 11:53:57 +0800

    add footer menus

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -16,8 +16,32 @@ const Footer = () => {
             <div className="flex flex-col items-center justify-between flex-1 mb-10 space-y-6 md:flex-row md:mb-0 md:space-y-0 text-grayishBlue">
               {/*Menus*/}
               <div className="flex flex-col space-y-4 md:flex-row md:ml-24 md:space-x-24 md:space-y-0">
-                <div className="flex flex-col space-y-4 text-center">
+                {/*Menu1*/}
+                <div className="flex flex-col space-y-4 text-center md:text-left">
                   {menu1Items.map((item: any, idx) => {
+                    return (
+                      <div key={idx}>
+                        <a href="#" className="hover:text-strongCyan">
+                          {item}
+                        </a>
+                      </div>
+                    );
+                  })}
+                </div>
+                {/*Menu2*/}
+                <div className="flex flex-col space-y-4 text-center md:text-left">
+                  {menu2Items.map((item, idx) => {
+                    return (
+                      <div key={idx}>
+                        <a href="#" className="hover:text-strongCyan">
+                          {item}
+                        </a>
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="flex flex-col space-y-4 text-center md:text-left">
+                  {menu3Items.map((item, idx) => {
                     return (
                       <div key={idx}>
                         <a href="#" className="hover:text-strongCyan">
