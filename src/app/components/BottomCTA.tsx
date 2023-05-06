21e22687870aac30d667fccb2ccb621bffb02e67
kenvilar <kenvilar@gmail.com>
2023-05-06 11:32:43 +0800

    update BottomCTA component

--- src/app/components/BottomCTA.tsx
+++ src/app/components/BottomCTA.tsx
@@ -1,7 +1,30 @@
 const BottomCTA = () => {
   return (
     <>
-      <div>BottomCTA</div>
+      <section id="bottom">
+        <div className="section-container my-20">
+          <h3>Clipboard for iOS and MacOS</h3>
+          <p className="section-content mb-10">
+            Available for free on the App Store. Download for Mac or iOS, sync
+            with iCloud and you{`'`}re ready to start adding to your clipboard.
+          </p>
+
+          <div className="button-container">
+            <a
+              href="#"
+              className="p-4 px-8 rounded-full shadow-lg bg-strongCyan duration-200 hover:opacity-80"
+            >
+              Download for iOS
+            </a>
+            <a
+              href="#"
+              className="p-4 px-8 rounded-full shadow-lg bg-lightBlue duration-200 hover:opacity-80"
+            >
+              Download for Mac
+            </a>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
